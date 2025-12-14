2c0eb945b8b8ab0befee4d6234ea8c9a3bc76a37
huutrong-le <huutrong0902@gmail.com>
2025-12-15 01:13:03 +0700

    update  cho server

---
+++ client/Components/Header.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+function Header() {
+   return <header className="px-10 py-6 bg-[#D7DEDC] text-gray-500 flex justify-between items-center">Header</header>;
+}
+
+export default Header;
--- client/context/jobsContext.js
+++ client/context/jobsContext.js
@@ -1,16 +1,21 @@
-"use client";
-import React, { createContext, use, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 import { useGlobalContext } from "./globalContext";
 import axios from "axios";
+import toast from "react-hot-toast";
+
 
 const JobsContext = createContext();
 
+axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.withCredentials = true;
+
 export const JobsContextProvider = ({ children }) => {
     const { userProfile } = useGlobalContext();
+
     const [jobs, setJobs] = useState([]);
     const [loading, setLoading] = useState(false);
     const [userJobs, setUserJobs] = useState([]);
- 
+
     const getJobs = async () => {
         setLoading(true);
         try {
@@ -23,23 +28,136 @@ export const JobsContextProvider = ({ children }) => {
         }
     };
 
-    const createJob = async (job) => {
+    const createJob = async (jobData) => {
         try {
-            const res = await axios.post("/api/v1/jobs", job);
+            const res = await axios.post("/api/v1/jobs", jobData);
+
+            toast.success("Job created successfully");
+
             setJobs((prevJobs) => [res.data, ...prevJobs]);
+
+            // update userJobs
+            if (userProfile._id) {
+                setUserJobs((prevUserJobs) => [res.data, ...prevUserJobs]);
+                await getUserJobs(userProfile._id);
+            }
+
+            await getJobs();
+            // redirect to the job details page
+            router.push(`/job/${res.data._id}`);
         } catch (error) {
             console.log("Error creating job", error);
         }
     };
 
+    const getUserJobs = async (userId) => {
+        setLoading(true);
+        try {
+            const res = await axios.get("/api/v1/jobs/user/" + userId);
+
+            setUserJobs(res.data);
+            setLoading(false);
+        } catch (error) {
+            console.log("Error getting user jobs", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const searchJobs = async (tags, location, title) => {
+        setLoading(true);
+
+        try {
+            // build query string
+            const query = new URLSearchParams();
+            if (tags) query.append("tags", tags);
+            if (location) query.append("location", location);
+            if (title) query.append("title", title);
+            // make request
+            const res = await axios.get("/api/v1/jobs/search?" + query.toString());
+            // set jobs to the response data
+            setJobs(res.data);
+            setLoading(false);
+        } catch (error) {
+            console.log("Error searching jobs", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+    //get jobs by id
+
+    const getJobById = async (id) => {
+        setLoading(true);
+        try {
+            const res = await axios.get(`/api/v1/jobs/${id}`);
+
+            setLoading(false);
+            return res.data;
+        } catch (error) {
+            console.log("Error getting job by id", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+    //like a job    
+    const likeJob = async (jobId) => {
+        try {
+            const res = axios.put(`/api/v1/jobs//like${jobId}`);
+            toast.success("Job liked successfully");
+            getJobs();
+        } catch (error) {
+            console.log("Error liking job", error);
+        }
+    };
+
+    //apply to a job
+    const applyToJob = async (jobId) => {
+        try {
+            const res = axios.put(`/api/v1/jobs/apply/${jobId}`);
+            toast.success("Applied to job successfully");
+            getJobs();
+        } catch (error) {
+            console.log("Error applying to job", error);
+        }
+    };
+    //delete a job
+    const deleteJob = async (jobId) => {
+        try {
+            await axios.delete(`/api/v1/jobs/${jobId}`);
+            setJobs((prevJobs) => prevJobs.filter((job) => job._id !== jobId));
+            searchJobs((prevJobs) => prevJobs.filter((job) => job._id !== jobId));
+            toast.success("Job deleted successfully");
+        } catch (error) {
+            console.log("Error deleting job", error);
+        }
+    };
+
     useEffect(() => {
         getJobs();
+
     }, []);
 
-    console.log("JobsContext - jobs:", jobs);
+    useEffect(() => {
+        if (userProfile._id) {
+            getUserJobs(userProfile._id);
+        }
+    }, [userProfile]);
+
+
 
     return (
-        <JobsContext.Provider value={{ jobs, loading, userJobs, getJobs }}>
+        <JobsContext.Provider value={{
+            jobs,
+            loading,
+            createJob,
+            userJobs,
+            searchJobs,
+            getJobById,
+            likeJob,
+            applyToJob,
+            deleteJob,
+        }}
+        >
             {children}
         </JobsContext.Provider>
     );
